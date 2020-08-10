af4ce23ad23c064eb3c909674bae9c98dbc66f2b
huyenlothi <69282948+huyenlothi@users.noreply.github.com>
2020-08-10 10:57:01 +0700

    javascript

---
+++ demo1.js
@@ -0,0 +1,9 @@
+// alert("xin chao");
+let ten = prompt("moi ban nhap ten");
+// alert(ten);
+let gioitinh = confirm("ban co phai la nu khong");
+//alert(gioitinh);
+//document.write("gioi tinh nu?" + gioitinh);
+document.getElementById("name").value = ten;
+//document.getElementById("gioitinh").innerText = gioitinh;
+console.log( ten + " " + gioitinh);
